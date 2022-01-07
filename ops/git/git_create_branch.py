3fc977e484a5c596405aad06feb5f0a23e937bd3
outrunJ <934260428@qq.com>
2022-01-07 10:35:36 +0800

    create_branch

--- ops/git/git_create_branch.py
+++ ops/git/git_create_branch.py
@@ -1,14 +1,20 @@
 import git_traverse
 from sys import argv
 
+# 处理入参
 rootpath = argv[1]
 source_branch_name = argv[2]
 branchname = argv[3]
-projectlist = argv[4]
+project_names = []
+if len(argv) > 4:
+    projectlist = argv[4]
+    project_names = projectlist.split(",")
 
+# 执行
 git_traverse.traverse(rootpath,
                       ["git checkout " + source_branch_name,
                        "git pull origin " + source_branch_name,
                        "git checkout -b " + branchname,
                        "git push origin " + branchname],
-                      projectlist.split(","))
+                      project_names
+                      )
